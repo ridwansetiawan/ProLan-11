62fed72b1208d41de84e0395d6121504ff762271
Ridwan Setiawan <onesosials@gmail.com>
2017-02-28 23:09:00 +0700

    Add files via upload

---
+++ Tugas2/mahasiswa.java
@@ -0,0 +1,7 @@
+public class mahasiswa {
+ public static void main(String argv []){
+	System.out.println("Nama : Ridwan Setiawan");
+	System.out.println("Nim : 10112133");
+	System.out.println("Alamat : KP.landean Girang");
+	}
+}
