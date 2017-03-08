cca84c4aa3775d92c5922cea66417eed66f7f4b3
Ridwan Setiawan <onesosials@gmail.com>
2017-03-08 18:18:36 +0700

    Add files via upload

---
+++ LatihanTypeData/Typedata.java
@@ -0,0 +1,60 @@
+public class Typedata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // type data objek
+        String nama = "Belajar Java";
+        System.out.println(nama.length());
+        
+        
+        //type data byte
+        byte maxbyte = 127;
+        byte minbyte = -128;
+        byte defaultByte = 0;
+        System.out.println(defaultByte);
+        
+        //type data short
+        short maxShort = 32767;
+        short minshort = -32768;
+        short defaultShort = 0;
+        System.out.println(defaultShort);
+        
+         //type data int
+        int maxInt = 214748364;
+        int minInt = - 2147483648;
+        int defaultInt = 0;
+        System.out.println(defaultInt);
+        
+         //type data long
+        long maxLong = 9223372036854775807L;
+        long minLong = -9223372036854775808L;
+        long defaultLong = 0L;
+        System.out.println(defaultLong);
+        
+         //type data float
+        float maxFloat = 3.40282347999999999999999999999999f;
+        float minFloat = -3.40282349999999999999999999999999f;
+        float defaultFloat = 0.0f;
+        System.out.println(defaultFloat);
+        
+         //type data double
+        double randomDouble = 1.797693134862315702d;
+        double randomDouble1 = -1.797693134862315602d;
+        double defaultDouble = 0.0;
+        System.out.println(randomDouble);
+        System.out.println(randomDouble);
+        
+        // type data boolean
+        boolean menikah = false;
+        System.out.println(menikah);
+        
+         // type data char
+        char A = 'A';
+        System.out.println(A);
+        
+        
+    }
+    
+}
